fce5c9178aa6b00d47bacaeaeda276929efeefc2
20005-ChintyaAnaTasya <100123162+20005-ChintyaAnaTasya@users.noreply.github.com>
2022-05-10 01:05:20 +0700

    Kelompok 2 kantin (final)

---
+++ admin/module/pengaturan/index.php
@@ -0,0 +1,23 @@
+ <!--sidebar end-->
+      
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+
+		  <div id="about" class="bgrnd">
+        <div class="container">
+        <div class="row about">
+          <div class="col">
+            
+          </div>
+            <div class="col-6">
+                <h1>About<br>Kala Senja Canteen</h1>
+                <h6>
+				Kala Senja Canteen merupakan kantin umum yang menyediakan makanan dan minuman dengan cepat dan praktis. Kantin ini sudah menggunakan teknologi 4.0. Meskipun kantin ini termasuk kategori modern, namun kantin ini menyediakan makanan dan minuman tradisional, sehingga pembeli bisa bebas memilih menu apa yang mereka suka
+				</h6>
+            </div>
+        </div>
+    </div>
